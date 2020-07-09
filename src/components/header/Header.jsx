751650d7cb468be7030817e0109a930bee4d3882
Felix Navas <navasfx19@gmail.com>
2020-07-09 06:29:36 -0600

    Add Bm structure

--- src/components/header/Header.jsx
+++ src/components/header/Header.jsx
@@ -1,19 +1,21 @@
 import React from "react";
-import image from "./logo-white.png";
+import image from "../../assets/images/logo-white.png";
 import "./Header.scss";
 
 function Header() {
   return (
     <header className="header">
-      <div className="logo-box">
-        <img src={image} alt="logo" className="logo" />
+      <div className="header__logo-box">
+        <img src={image} alt="logo" className="header__logo" />
       </div>
-      <div className="text-box">
+      <div className="header__text-box">
         <h1 className="heading-primary">
-          <span className="heading-primary-main">Outdoors</span>
-          <span className="heading-primary-sub">is where life happens</span>
+          <span className="heading-primary--main">Outdoors</span>
+          <span className="heading-primary--sub">is where life happens</span>
         </h1>
-        <button className="btn btn-white btn-animated">Discover our tours</button>
+        <a href="/" className="btn btn--white btn--animated">
+          Discover our tours
+        </a>
       </div>
     </header>
   );
