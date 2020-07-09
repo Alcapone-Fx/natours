8a68e0c6c8f609c6f445883e56f6556fb7d8c1f9
Felix Navas <navasfx19@gmail.com>
2020-07-09 06:42:12 -0600

    Refactor

--- src/index.js
+++ src/index.js
@@ -1,12 +1,10 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './main.scss';
-import App from './components/main/App';
+import React from "react";
+import ReactDOM from "react-dom";
+import App from "./components/main/App";
 
 ReactDOM.render(
   <React.StrictMode>
     <App />
   </React.StrictMode>,
-  document.getElementById('root')
+  document.getElementById("root")
 );
-
