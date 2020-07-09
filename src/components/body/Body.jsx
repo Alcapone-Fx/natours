4a3258a7fa326b22e1fdce3b00f68b4d0b71cb87
Felix Navas <navasfx19@gmail.com>
2020-07-09 06:44:45 -0600

    Body component

---
+++ src/components/body/Body.jsx
@@ -0,0 +1,43 @@
+import React from "react";
+
+const Body = () => {
+  return (
+    <main>
+      <section className="section-about">
+        <div className="u-center-text u-margin-bottom-big">
+          <h2 className="heading-secondary">
+            Exciting tours for adventurous people
+          </h2>
+        </div>
+        <div className="row">
+          <div className="col-1-of-2">
+            <h3 className="heading-terciary u-margin-bottom-small">
+              You're going to fall in love with nature
+            </h3>
+            <p className="paragraph">
+              Lorem ipsum dolor sit amet consectetur, adipisicing elit. Aut,
+              perferendis, libero ipsa assumenda praesentium repellendus
+              possimus sapiente distinctio vitae sunt ut delectus velit qui.
+              Voluptatem quam consequuntur at culpa laudantium!
+            </p>
+
+            <h3 className="heading-terciary u-margin-bottom-small">
+              Live adventures like you never have before
+            </h3>
+            <p className="paragraph">
+              Lorem ipsum dolor sit amet consectetur, adipisicing elit. Aut,
+              perferendis, libero ipsa assumenda.
+            </p>
+
+            <a href="/" className="btn-text">
+              Learn more &rarr;
+            </a>
+          </div>
+          <div className="col-1-of-2">Col 1 of 2</div>
+        </div>
+      </section>
+    </main>
+  );
+};
+
+export default Body;
