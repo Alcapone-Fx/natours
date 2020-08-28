3fa0a71fe37512a5d51ffc4c0eb7a9fd948e3294
Felix Navas <navasfx19@gmail.com>
2020-08-28 14:28:35 -0600

    :sparkles: Add sections to body

--- src/components/body/Body.jsx
+++ src/components/body/Body.jsx
@@ -1,11 +1,19 @@
 import React from "react";
 
 import SectionAbout from "../sectionAbout/SectionAbout";
+import SectionFeature from "../sectionFeatures/SectionFeatures";
+import SectionTours from "../sectionTours/SectionTours";
+import Stories from "../sectionStories/sectionStories";
+import SectionBooking from "../sectionBooking/SectionBooking";
 
 const Body = () => {
   return (
     <main>
       <SectionAbout />
+      <SectionFeature />
+      <SectionTours />
+      <Stories />
+      <SectionBooking />
     </main>
   );
 };
