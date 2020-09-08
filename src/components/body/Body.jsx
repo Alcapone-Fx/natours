35e933ece05210caea09947d4a4d99628a92b585
Felix Navas <navasfx19@gmail.com>
2020-09-08 11:45:37 -0600

    :sparkles: Add footer

--- src/components/body/Body.jsx
+++ src/components/body/Body.jsx
@@ -5,16 +5,20 @@ import SectionFeature from "../sectionFeatures/SectionFeatures";
 import SectionTours from "../sectionTours/SectionTours";
 import Stories from "../sectionStories/sectionStories";
 import SectionBooking from "../sectionBooking/SectionBooking";
+import Footer from "../footer/Footer";
 
 const Body = () => {
   return (
-    <main>
-      <SectionAbout />
-      <SectionFeature />
-      <SectionTours />
-      <Stories />
-      <SectionBooking />
-    </main>
+    <>
+      <main>
+        <SectionAbout />
+        <SectionFeature />
+        <SectionTours />
+        <Stories />
+        <SectionBooking />
+      </main>
+      <Footer />
+    </>
   );
 };
 
