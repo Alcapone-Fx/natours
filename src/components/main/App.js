36e00e9b551f5dc0ee79a231d767d6a608abb115
Felix Navas <navasfx19@gmail.com>
2020-09-08 18:12:46 -0600

    :sparkles: Add Navigation component to page

--- src/components/main/App.js
+++ src/components/main/App.js
@@ -1,5 +1,6 @@
 import React from "react";
 
+import Navigation from "../navigation/Navigation";
 import Header from "../header/Header";
 import Body from "../body/Body";
 import "../../assets/sass/main.scss";
@@ -7,6 +8,7 @@ import "../../assets/sass/main.scss";
 function App() {
   return (
     <div className="App">
+      <Navigation />
       <Header />
       <Body />
     </div>
