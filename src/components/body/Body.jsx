9ecb96a805548ca0b061b81e1707b41241b38e76
Felix Navas <navasfx19@gmail.com>
2020-09-09 10:10:08 -0600

    :sparkles: Add popUp to Body.jsx

--- src/components/body/Body.jsx
+++ src/components/body/Body.jsx
@@ -5,11 +5,13 @@ import SectionFeature from "../sectionFeatures/SectionFeatures";
 import SectionTours from "../sectionTours/SectionTours";
 import Stories from "../sectionStories/sectionStories";
 import SectionBooking from "../sectionBooking/SectionBooking";
+import PopUp from "../popUp/PopUp";
 import Footer from "../footer/Footer";
 
 const Body = () => {
   return (
     <>
+      <PopUp />
       <main>
         <SectionAbout />
         <SectionFeature />
